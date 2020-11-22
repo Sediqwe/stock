74e4ed418eec3daf036f911f2f8f8c1b8bbf4c73
Sediqwe <sediqwe@gmail.com>
2020-11-22 14:25:27 +0100

    képfeltöltés rész

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -1,14 +1,30 @@
 class ImagesController < ApplicationController
   def index
+    @images = Image.all
   end
 
   def new
+    @image = Image.new
   end
 
   def create
+    @image = Image.new(image_params)
+
+    if @image.save
+      redirect_to @image
+    else
+      render :new
+    end
   end
 
   def update
+    @image = Image.find(params[:id])
+
+    if @image.update(image_params)
+      redirect_to @image
+    else
+      render :edit
+    end
   end
 
   def show
@@ -16,5 +32,13 @@ class ImagesController < ApplicationController
 
   def destroy
   end
-  
+
+  def edit
+    @image = Image.find(params[:id])
+  end
+
+  private
+    def image_params
+      params.require(:image).permit(:title, :description)
+    end
 end
