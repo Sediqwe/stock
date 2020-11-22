15e9389815ae35449ca10f20e5c64fb7de796b38
Sediqwe <sediqwe@gmail.com>
2020-11-22 13:18:53 +0100

    Második

---
+++ app/controllers/images_controller.rb
@@ -0,0 +1,20 @@
+class ImagesController < ApplicationController
+  def index
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def update
+  end
+
+  def show
+  end
+
+  def destroy
+  end
+  
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  resources :images
+  get 'users', to: 'users#index'
+  devise_for :users
   root "welcome#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
