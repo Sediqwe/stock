0bc868cbef773c377a34121c07da25802fbd818b
Sediqwe <sediqwe@gmail.com>
2020-12-07 22:30:25 +0100

    Apoca lecsap2

--- app/controllers/apocas_controller.rb
+++ app/controllers/apocas_controller.rb
@@ -7,4 +7,22 @@ class ApocasController < ApplicationController
 
   def show
   end
+
+  def new
+    @apoca = Apoca.new
+  end
+
+  def create   
+    @apoca = Apoca.new(apoca_params)
+    if @apoca.save
+      render 'new'
+    else
+      render 'new'
+    end
+    
+  end
+  private
+  def apoca_params
+    params.require(:apoca).permit(:user, :darab)
+  end
 end
