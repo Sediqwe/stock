a9b66f6f2d1012c60a6268e00d2b2598b66a5ba1
Sediqwe <sediqwe@gmail.com>
2021-01-10 19:00:01 +0100

    Youtube linkek , rendezés

--- app/controllers/youtubes_controller.rb
+++ app/controllers/youtubes_controller.rb
@@ -4,7 +4,7 @@ class YoutubesController < ApplicationController
   # GET /youtubes
   # GET /youtubes.json
   def index
-    @youtubes = Youtube.all
+    @youtubes = Youtube.paginate(page: params[:page], per_page: 10).order(id: :desc)
   end
 
   # GET /youtubes/1
