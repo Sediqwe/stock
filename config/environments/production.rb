e5c80cdda06f5c23ac8e577063aef0c235ae5f6e
Sediqwe <sediqwe@gmail.com>
2021-04-01 19:59:19 +0200

    System update

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,6 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-  config.hosts << "pittyu.sediqwe.eu"
+  config.hosts << "gtav.sediqwe.eu"
 
   # Code is not reloaded between requests.
   config.cache_classes = true
