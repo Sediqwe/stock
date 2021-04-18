e690fe3e099e3000a499d9fb34f92c04a8ea7fbe
Sediqwe <sediqwe@gmail.com>
2021-04-18 20:47:35 +0200

    First Upload to DB 1.1

--- app/controllers/translater_controller.rb
+++ app/controllers/translater_controller.rb
@@ -1,5 +1,8 @@
 class TranslaterController < ApplicationController
   def index
     @translater = Translate.all.last(200)
+    @translate_all = Translate.all.size
+    @translate_real_all = Translate.where("length(original) > 0").size
+    
   end
 end
