88c46c6e285b3d9c8790b0f74f751147088af688
Sediqwe <sediqwe@gmail.com>
2021-07-03 18:49:20 +0200

    Megtranslate_update

--- app/helpers/teszt_helper.rb
+++
@@ -1,2 +0,0 @@
-module TesztHelper
-end
--- test/controllers/teszt_controller_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class TesztControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get teszt_index_url
-    assert_response :success
-  end
-
-end
