14c8fa6ae59048d56f8af538701548d6f4de816d
Sediqwe <sediqwe@gmail.com>
2021-07-03 22:41:18 +0200

    Translate 0.6.1

--- app/controllers/translater_controller.rb
+++ app/controllers/translater_controller.rb
@@ -1,11 +1,11 @@
 class TranslaterController < ApplicationController
   def index
-    @translater = Translate.where("original ~ ?", '^(((?!_|[0-9])|[a-z].*).)*$')
+    @translater = Translate.where("original ~ ?", '^(((?!_)|[a-z].*).)*$').limit(200)
     #@translater = Translate.where('original ~ :pat', :pat => "^(((?!_)|[a-z].*).)*$")
     
     
     @translate_all = Translate.all.size
-    @translate_real_all =  Translate.where("original ~ ?", '^(((?!_|[0-9])|[a-z].*).)*$').size
+    @translate_real_all =  Translate.where("original ~ ?", '^(((?!_)|[a-z].*).)*$').size
     
   end
   def toroldlegyszi
