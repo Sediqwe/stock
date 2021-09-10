5f943e78bffbf088e7d45ea94058ef479dc01b89
Sediqwe <sediqwe@gmail.com>
2021-09-10 23:52:17 +0200

    CS Sok.1.11

--- app/controllers/translater_controller.rb
+++ app/controllers/translater_controller.rb
@@ -3,9 +3,9 @@ class TranslaterController < ApplicationController
   def index
     if params[:id].present?
       if params[:col].present?
-        @translater = Translate.where(trans_type: false, project_id: params[:id], file: params[:file], col_num: params[:col]).paginate(page: params[:page], per_page: 1).order(row_num: :ASC, col_num: :ASC).where("length(original) > 2")
+        @translater = Translate.where(trans_type: false, project_id: params[:id], file: params[:file], col_num: params[:col]).paginate(page: params[:page], per_page: 1).order(row_num: :ASC, col_num: :ASC).where("length(original) > 0")
       else
-        @translater = Translate.where(trans_type: false, project_id: params[:id], file: params[:file]).paginate(page: params[:page], per_page: 10)
+        @translater = Translate.where(trans_type: false, project_id: params[:id], file: params[:file]).paginate(page: params[:page], per_page: 1)
       end
         @translate_all = Translate.where(project_id: params[:id]).size
         @translate_real_all =  Translate.where(project_id: params[:id]).where("original ~ ?", '^(((?!_)(?!^\d+$)|[a-z]?![0-9].*).)*$').size
